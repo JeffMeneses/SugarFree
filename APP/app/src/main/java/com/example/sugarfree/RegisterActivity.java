95d3ff10ad5612c2a5ed96b0e9d6c284edb9f158
Jefferson Meneses <jeffersonmeneses98@gmail.com>
2021-05-16 14:59:32 -0300

    Adding Register Screen

--- APP/app/src/main/java/com/example/sugarfree/RegisterActivity.java
+++ APP/app/src/main/java/com/example/sugarfree/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.sugarfree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onClickConfirm(View v)
+    {
+        //TODO: validar dados de cadastro
+    }
+
+    public void onClickAddPicture(View v)
+    {
+        //TODO: abrir galeria para escolher foto
+    }
+
+    public void onClickReturn(View v){
+        finish();
     }
 }
