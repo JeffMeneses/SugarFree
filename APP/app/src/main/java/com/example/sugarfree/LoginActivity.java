04266a114c2a74816c2c7fd2a86df558eda370b9
Jefferson Meneses <jeffersonmeneses98@gmail.com>
2021-05-16 14:45:31 -0300

    Adding Login Screen

--- APP/app/src/main/java/com/example/sugarfree/LoginActivity.java
+++ APP/app/src/main/java/com/example/sugarfree/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.sugarfree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +12,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void onClickLogin(View v)
+    {
+        //TODO: validar credenciais do login
+    }
+
+    public void onClickReturn(View v){
+        finish();
+    }
+
 }
