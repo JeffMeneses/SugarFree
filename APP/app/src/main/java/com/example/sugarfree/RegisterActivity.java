c05b08ee318f2bca169442aa308ce3b6f38230ad
Jefferson Meneses <jeffersonmeneses98@gmail.com>
2021-05-16 18:14:40 -0300

    Adding validations for register account

--- APP/app/src/main/java/com/example/sugarfree/RegisterActivity.java
+++ APP/app/src/main/java/com/example/sugarfree/RegisterActivity.java
@@ -1,21 +1,101 @@
 package com.example.sugarfree;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sugarfree.utils.Constants;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Context mContext;
+
+    private TextView mEmail, mName, mPassword, mConfirmPassword;
+    private  ImageView mPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mContext = getApplicationContext();
+
+        mEmail = findViewById(R.id.txtEmail);
+        mName = findViewById(R.id.txtName);
+        mPassword = findViewById(R.id.txtPassword);
+        mConfirmPassword = findViewById(R.id.txtConfirmPassword);
+
+        mPicture = (ImageView)findViewById(R.id.profilePicture);
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.ic_profile_picture);
+        mPicture.setImageBitmap(image);
     }
 
     public void onClickConfirm(View v)
     {
-        //TODO: validar dados de cadastro
+        String email = mEmail.getText().toString();
+        String name = mName.getText().toString();
+        String password = mPassword.getText().toString();
+        String confirmPassword = mConfirmPassword.getText().toString();
+
+        if(!checkInputs(email, name, password, confirmPassword)) {
+            return;
+        }
+
+        //TODO: fazer requisição de cadastro
+        if(true)
+        {
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setTitle("Aviso");
+            dlgAlert.setMessage("Conta criada com sucesso!");
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }
+    }
+
+    public boolean checkInputs(String email, String name, String password, String confirmPassword)
+    {
+        boolean validInput = true;
+        StringBuilder errorMessage = new StringBuilder();
+
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            validInput = false;
+            errorMessage.append(Constants.MSG_INVALID_EMAIL);
+        }
+        else if(name.isEmpty() || name.length() < Constants.MIN_NAME_LENGTH)
+        {
+            validInput = false;
+            errorMessage.append(Constants.MSG_INVALID_NAME);
+        }
+        else if(password.isEmpty() || password.length() < Constants.MIN_PASSWORD_LENGTH)
+        {
+            validInput = false;
+            errorMessage.append(Constants.MSG_INVALID_PASSWORD);
+        }
+        else if(!password.equals(confirmPassword))
+        {
+            validInput = false;
+            errorMessage.append(Constants.MSG_PASSWORD_MISMATCH);
+        }
+
+        if(!validInput)
+        {
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setTitle(Constants.ALERT_INVALID_INPUT);
+            dlgAlert.setMessage(errorMessage);
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }
+
+        return validInput;
     }
 
     public void onClickAddPicture(View v)
---
+++ APP/app/src/main/java/com/example/sugarfree/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.example.sugarfree.utils;
+
+public class Constants {
+    public static final String ALERT_INVALID_INPUT = "Entrada inválida!";
+    public static final String MSG_INVALID_EMAIL = "Email inválido. Seu email parece não seguir o padrão correto\n";
+    public static final String MSG_INVALID_NAME = "Nome inválido. Seu nome deve conter pelomenos"+ Constants.MIN_NAME_LENGTH +"caracteres\n";
+    public static final String MSG_INVALID_PASSWORD = "Senha inválida. Sua senha deve conter pelomenos"+ Constants.MIN_PASSWORD_LENGTH +"caracteres.\n";
+    public static final String MSG_PASSWORD_MISMATCH = "As senhas devem coincidir.";
+
+    public static final int MIN_NAME_LENGTH = 3;
+    public static final int MIN_PASSWORD_LENGTH = 3;
+}
