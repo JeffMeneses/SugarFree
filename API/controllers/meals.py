2a4839fd4ac1610419cfaef72400b22578015b5d
Jefferson Meneses <jeffersonmeneses98@gmail.com>
2021-06-06 19:53:57 -0300

    [API] Implementing new routes for Meals

---
+++ API/controllers/meals.py
@@ -0,0 +1,165 @@
+from flask import Flask, jsonify, request
+from app import app, db
+import json
+from bson import json_util
+import uuid
+
+# PLACE HOLDER
+meals_db = [
+    {
+        "idUser": 1,
+        "name": "José apenas",
+        "mealData": 
+        {
+            "morningSnack": [
+                
+            ],
+            "breakfast": [
+                {
+                    "foodName": "cenoura",
+                    "quantity": "50",
+                    "unit": "grama(s)"
+                },
+                {
+                    "foodName": "leite",
+                    "quantity": "500",
+                    "unit": "mililitro(s)"
+                }
+            ],
+            "lunch": [
+                {
+                    "foodName": "arroz",
+                    "quantity": "300",
+                    "unit": "grama(s)"
+                },
+                {
+                    "foodName": "carne",
+                    "quantity": "300",
+                    "unit": "grama(s)"
+                }
+            ],
+            "afternoonSnack": [
+                
+            ],
+            "dinner": [
+                {
+                    "foodName": "cenoura",
+                    "quantity": "50",
+                    "unit": "gramas"
+                },
+                {
+                    "foodName": "leite",
+                    "quantity": "500",
+                    "unit": "mililitro(s)"
+                }
+            ],
+            "eveningSnack": [
+                
+            ]
+        }
+    },
+    {
+        "idUser": 2,
+        "name": "Julia apenas",
+        "mealData": 
+        {
+            "morningSnack": [
+                
+            ],
+            "breakfast": [
+                {
+                    "foodName": "brócolis",
+                    "quantity": "50",
+                    "unit": "grama(s)"
+                },
+                {
+                    "foodName": "suco",
+                    "quantity": "500",
+                    "unit": "mililitro(s)"
+                }
+            ],
+            "lunch": [
+                {
+                    "foodName": "feijão",
+                    "quantity": "300",
+                    "unit": "grama(s)"
+                },
+                {
+                    "foodName": "frango",
+                    "quantity": "300",
+                    "unit": "grama(s)"
+                }
+            ],
+            "afternoonSnack": [
+                
+            ],
+            "dinner": [
+                {
+                    "foodName": "cenoura",
+                    "quantity": "50",
+                    "unit": "gramas"
+                },
+                {
+                    "foodName": "leite",
+                    "quantity": "500",
+                    "unit": "mililitro(s)"
+                }
+            ],
+            "eveningSnack": [
+                
+            ]
+        }
+    }
+]
+
+#@app.route('/mealCategory/<string:mealName>/<string:idUser>', methods=['GET'])
+#def mealCategory(mealName, idUser):
+#    meals_list  = list(db.meals.find({"idUser": idUser}))
+#    return json.dumps(meals_list, default=json_util.default)
+
+@app.route('/meals', methods=['POST'])
+def meals():
+    if request.method == 'POST':
+        meal = {
+        "idUser": request.json.get('idUser'),
+        "name": request.json.get('name'),
+        "morningSnack": [],
+        "breakfast": [],
+        "lunch": [],
+        "afternoonSnack": [],
+        "dinner": [],
+        "eveningSnack": []
+    }
+
+        if db.meals.insert_one(meal):
+            return jsonify({"success": "Refeição inserida com sucesso.", "statusCode": 200}), 200
+
+        return jsonify({"error": "A inserção de refeição falhou.", "statusCode": 400}), 400
+
+@app.route('/food', methods=['POST'])
+def food():
+
+    mealUpdate = db.meals.update_one(
+    {"idUser": request.json.get('idUser')}, 
+    {
+        "$push": {
+            request.json.get('mealName'): 
+            {
+                "foodName": request.json.get('foodName'),
+                "quantity": request.json.get('quantity'),
+                "unit": request.json.get('unit')
+            }
+        }
+    })
+
+    if mealUpdate.modified_count:
+        return jsonify({"success": "Alimento inserido com sucesso.", "statusCode": 200}), 200
+
+    return jsonify({"error": "A inserção de alimento falhou.", "statusCode": 400}), 400
+
+@app.route('/mealCategory/<int:idUser>/<string:category>', methods=['GET'])
+def mealCategory(idUser, category):
+    meals_list  = list(db.meals.find({"idUser": idUser}))
+    categoryResponse = json.loads(json.dumps(meals_list, default=json_util.default))
+
+    return jsonify(categoryResponse[0][category])
